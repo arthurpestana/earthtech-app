0b3d5c662552937a9f2b73f5cba5db3973d958b5
Arthur Pestana <92184381+arthurpestana@users.noreply.github.com>
2024-03-17 18:26:49 -0300

    main-app

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { StatusBar } from "react-native";
 
 import { NavigationContainer }  from '@react-navigation/native'
-import Routes from './src/routes'
+import Routes from './src/routes/index'
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <StatusBar backgroundColor={"#38A69D"} barStyle={"light-content"}/>
-            <Routes></Routes>
+        <NavigationContainer>   
+            <Routes/>
         </NavigationContainer>
     )
 }
