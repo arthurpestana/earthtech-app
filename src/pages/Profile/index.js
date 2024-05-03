2eaaf9b5e563fcdd3f3ace31fa7fbd102888104e
Arthur Pestana <92184381+arthurpestana@users.noreply.github.com>
2024-05-02 22:35:18 -0300

    profile style

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,31 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView, View, Text, Image, TouchableOpacity } from 'react-native'
+import { useMQTT } from "../../components/Context";
+import { useSQLiteContext } from "expo-sqlite/next";
+import { useNavigation } from '@react-navigation/native'
+import styles from '../../styles/style_profile'
+import * as Animatable from 'react-native-animatable'
+import { AntDesign, Entypo, Feather} from '@expo/vector-icons'
 
 export default function Profile() {
+    const Navigation = useNavigation()
+    const db = useSQLiteContext()
+    const [mail, setMail] = useState('')
+    const { userId, setId, userName, setName, loggedIn, setLoggedIn, client, setClient, isConnected, setConnected } = useMQTT()
+    
+    async function loginAccount(){
+        element = await db.getAllAsync(`SELECT * FROM users WHERE id = ${userId}`)
+        console.log(element[0])
+        setMail(element[0].email)
+    }
+
+    async function LogoutAccount(){
+        await db.execAsync(`UPDATE users SET logged = ${0} WHERE id = ${userId}`)
+        setName('')
+        setId('')
+        setMail('')
+        setConnected(false)
+        setClient('')
+        setLoggedIn(false)
+        Navigation.navigate('Welcome')
+    }
+
+
+    useEffect(() => {
+        loginAccount()
+    }, [])
+
     return(
-        <SafeAreaView>
-            <View>
-                <View>
-                    <TouchableOpacity  onPress={() => {}}>
-                        <Animated.View>
-                            <AntDesign name='plus' size={24} color="#000" style={{transform: [{rotate: '45deg'}]}}/>
-                        </Animated.View>
+        <SafeAreaView style={styles.container__profile}>
+            <View style={styles.container__header}>
+                <View style={[styles.header__close_button]}>
+                    <TouchableOpacity style={[styles.button]} onPress={() => Navigation.navigate('StatusInformation')}>
+                        <AntDesign name='plus' size={24} color="hsl(228, 8%, 98%)" style={{transform: [{rotate: '45deg'}]}}/>
                     </TouchableOpacity>
                 </View>
             </View>
-            <View>
-                <View>
-                    <View>
-                        <Image></Image>
+            <View style={styles.container__main}>
+                <View style={styles.main__profile}>
+                    <View style={styles.profile__image}>
+                        <View style={styles.logo_image}>
+                            <Text style={styles.logo_text}>{userName[0]}</Text>
+                        </View>
                     </View>
-                    <View>
-                        <Text></Text>
-                        <Text></Text>
+                    <View style={styles.profile__info}>
+                        <Text style={styles.profile_name}>{userName}</Text>
+                        <Text style={styles.profile_email}>{mail}</Text>
                     </View>
                 </View>
-                <View>
-                    <TouchableOpacity>
-                        x
+                <View style={styles.main__logout}>
+                    <TouchableOpacity style={styles.logout__button} onPress={() => LogoutAccount()}>
+                        <Text style={styles.logout__text}>Desconectar</Text>
+                        <Feather name='log-out' size={18} color={'hsl(228, 8%, 98%)'}/>
                     </TouchableOpacity>
                 </View>
             </View>
---
+++ src/styles/style_profile.js
@@ -0,0 +1,100 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container__profile: {
+        flex: 1,
+        width: "100%",
+        height: '100%',
+        backgroundColor: 'hsl(228, 6%, 8%)',
+        paddingHorizontal: '10%'
+    },
+
+    container__header: {
+        position: 'absolute',
+        top: 40,
+        left: 30,
+    },
+
+    container__main: {
+        display: 'flex',
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: 'flex-start',
+        justifyContent: 'space-around',
+        flexDirection: 'column'
+    },
+
+    main__profile: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
+
+    logo_image: {
+        width: 100,
+        height: 100,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'hsl(93, 40%, 30%)',
+        borderRadius: 200
+    },
+
+    logo_text: {
+        fontFamily: 'Montserrat_400Regular',
+        color: 'hsl(228, 8%, 98%)',
+        textTransform: 'capitalize',
+        fontSize: 60
+    },
+
+    profile__info: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        width: '100%',
+        marginLeft: "10%",
+        gap: 10
+    },
+
+    profile_name: {
+        fontFamily: 'Montserrat_600SemiBold',
+        color: 'hsl(228, 8%, 98%)',
+        textTransform: 'capitalize',
+        fontSize: 22
+    },
+
+    profile_email: {
+        fontFamily: 'Montserrat_400Regular',
+        color: 'hsl(228, 8%, 70%)',
+        textTransform: 'lowercase',
+        fontSize: 14
+    },
+
+    
+    main__logout: {
+        width: '100%',
+        alignItems: 'flex-end',
+    },
+
+    logout__button: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        borderRadius: 10,
+        backgroundColor: 'hsl(93, 40%, 30%)',
+        gap: 10
+    },
+
+    logout__text: {
+        fontFamily: 'Montserrat_400Regular',
+        color: 'hsl(228, 8%, 98%)',
+        fontSize: 16
+    }
+})
