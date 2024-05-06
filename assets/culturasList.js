9716759a497975d66178e25b4acad6f11001c79c
Arthur Pestana <92184381+arthurpestana@users.noreply.github.com>
2024-05-06 13:45:38 -0300

    Lista de culturas

---
+++ assets/culturasList.js
@@ -0,0 +1,69 @@
+const culturas = [
+    {
+        nome: "Alface",
+        tipo: "Folha",
+        clima: "Temperado",
+        solo: ["Argiloso", "Siltoso"],
+        necessidadeDeAgua: "Alta",
+        tempoDeCrescimento: "Curto"
+    },
+    {
+        nome: "Tomate",
+        tipo: "Fruto",
+        clima: "Tropical",
+        solo: ["Argiloso", "Arenoso"],
+        necessidadeDeAgua: "Média",
+        tempoDeCrescimento: "Médio"
+    },
+    {
+        nome: "Cenoura",
+        tipo: "Raiz",
+        clima: "Temperado",
+        solo: ["Argiloso", "Areia"],
+        necessidadeDeAgua: "Baixa",
+        tempoDeCrescimento: "Longo"
+    },
+    {
+        nome: "Pimentão",
+        tipo: "Fruto",
+        clima: "Tropical",
+        solo: ["Argiloso", "Areia"],
+        necessidadeDeAgua: "Média",
+        tempoDeCrescimento: "Médio"
+    },
+    {
+        nome: "Morango",
+        tipo: "Fruto",
+        clima: "Temperado",
+        solo: ["Argiloso", "Arenoso"],
+        necessidadeDeAgua: "Alta",
+        tempoDeCrescimento: "Longo"
+    },
+    {
+        nome: "Feijão",
+        tipo: "Leguminosa",
+        clima: "Tropical",
+        solo: ["Argiloso", "Siltoso"],
+        necessidadeDeAgua: "Média",
+        tempoDeCrescimento: "Médio"
+    },
+    {
+        nome: "Batata",
+        tipo: "Tuberosa",
+        clima: "Temperado",
+        solo: ["Argiloso", "Areia"],
+        necessidadeDeAgua: "Média",
+        tempoDeCrescimento: "Longo"
+    },
+    {
+        nome: "Abobrinha",
+        tipo: "Fruto",
+        clima: "Tropical",
+        solo: ["Argiloso", "Arenoso"],
+        necessidadeDeAgua: "Média",
+        tempoDeCrescimento: "Curto"
+    }
+];
+    
+
+export default culturas
