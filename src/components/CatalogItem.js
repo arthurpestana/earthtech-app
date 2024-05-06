1eba14e6527f0ff62d90295900c98f13a7190f60
Arthur Pestana <92184381+arthurpestana@users.noreply.github.com>
2024-05-06 13:46:28 -0300

    CatalogItems quase pronto

--- src/components/CatalogItem.js
+++ src/components/CatalogItem.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { View, TouchableOpacity, Text, StyleSheet, SafeAreaView, Image } from 'react-native'
 import { Feather } from '@expo/vector-icons'
+import * as Animatable from 'react-native-animatable'
 
-export default function Catalog() {
+export default function Catalog(props) {
     return (
-        <SafeAreaView style={styles.dashboard__items}>
+        <Animatable.View animation={'fadeInLeft'} delay={600} style={styles.dashboard__items}>
             <View style={styles.item__div_img}>
                 <Feather name='slack' size={40} color={'hsl(228, 8%, 98%)'}/>
             </View>
             <View style={styles.dashboard__dados}>
                 <View style={styles.item__div_info}>
-                    <Text style={styles.item__title}>Teste</Text>
-                    <Text style={styles.item__text}>Teste • Teste</Text> 
+                    <Text style={styles.item__title}>{props.nome}</Text>
+                    <Text style={styles.item__text}>{props.tipo} • {props.clima}</Text> 
                 </View>
             </View>
-        </SafeAreaView>
+            <View style={styles.dashboard__dados}>
+                <TouchableOpacity>
+                    <Feather name='chevron-right' size={25} color={'hsl(228, 8%, 98%)'}/>
+                </TouchableOpacity>
+            </View>
+        </Animatable.View>
     )
 }
 
@@ -24,7 +30,7 @@ const styles = StyleSheet.create({
         height: 100,
         backgroundColor: "hsl(228, 6%, 12%)",
         borderRadius: 20,
-        marginTop: '5%',
+        marginBottom: '5%',
         alignItems: 'center',
         flexDirection: 'row',
         justifyContent: 'space-around'
@@ -47,7 +53,7 @@ const styles = StyleSheet.create({
 
     item__div_info: {
         display: 'flex',
-        alignItems: 'flex-start',
+        alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'column',
         gap: 5
